66f889d17561693e0367d191c4176beb40579bcc
Peishi Dong <pdong@f0r4s5.codam.nl>
2023-04-29 14:59:59 +0200

    ex00 done

---
+++ 4-CPP Part 1/Module4/ex00/Animal.cpp
@@ -0,0 +1,30 @@
+#include "Animal.hpp"
+
+Animal::Animal(void): type("Animal")
+{
+	std::cout << "Animal constructor called.\n";
+}
+
+Animal::~Animal(void)
+{
+	std::cout << "Animal destructor called.\n";
+}
+
+Animal::Animal(Animal const &other): type(other.type)
+{}
+
+Animal &Animal::operator=(Animal const &other)
+{
+	this->type = other.type;
+	return (*this);
+}
+
+void	Animal::makeSound(void) const
+{
+	std::cout << "Realistic " << type << " sounds.\n";
+}
+
+std::string	Animal::getType(void) const
+{
+	return type;
+}
---
+++ 4-CPP Part 1/Module4/ex00/WrongAnimal.cpp
@@ -0,0 +1,30 @@
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(void): type("WrongAnimal")
+{
+	std::cout << "WrongAnimal constructor called.\n";
+}
+
+WrongAnimal::~WrongAnimal(void)
+{
+	std::cout << "WrongAnimal destructor called.\n";
+}
+
+WrongAnimal::WrongAnimal(WrongAnimal const &other): type(other.type)
+{}
+
+WrongAnimal &WrongAnimal::operator=(WrongAnimal const &other)
+{
+	this->type = other.type;
+	return (*this);
+}
+
+void	WrongAnimal::makeSound(void) const
+{
+	std::cout << "Realistic " << type << " sounds.\n";
+}
+
+std::string	WrongAnimal::getType(void) const
+{
+	return type;
+}
