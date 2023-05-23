7ff7d2d164c301e453542985e511b92ff7ddf68d
Peishi Dong <pdong@f0r5s7.codam.nl>
2023-05-23 15:56:07 +0200

    fixed the header file, ex02 done.

--- 5-CPP Part 2/Module8/ex02/MutantStack.hpp
+++ 5-CPP Part 2/Module8/ex02/MutantStack.hpp
@@ -1,29 +1,24 @@
 #pragma once
 #include <iostream>
 #include <algorithm>
-#include <stdexcept>
+#include <stack>
 #include <vector>
 
 template<typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack() {}
-	~MutantStack() {}
-	MutantStack(const MutantStack &other) {*this = copy;}
-	MutantStack &operator=(const MutantStack &other) {*this = other; return *this;}
-	
 	typedef T	*iterator;
 	iterator	begin(void) 
 	{
 		iterator ret = &this->top();
-		for (unsigned int i = 0; i < size(); i++)
+		for (unsigned int i = 0; i < this->size(); i++)
 			ret--;
 		return ret;
 	}
 	iterator	end(void) 
 	{
-		return (top() + 1);
+		return (&this->top() + 1);
 	}
 private:
 };
